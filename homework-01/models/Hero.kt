4119f56c73062b465b530bda4eb97b00690e9513
Алина Чернявская <aiponalinachernavskaa@gmail.com>
2024-10-14 14:22:10 +0600

    Вливаю код

---
+++ homework-01/models/Hero.kt
@@ -0,0 +1,7 @@
+package com.example.homework01.models
+
+data class Hero(
+    val name: String,
+    val description: String,
+    val imageUrl: String,
+)
---
+++ homework-01/navigation/RootController.kt
@@ -0,0 +1,34 @@
+package com.example.homework01.navigation
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.homework01.models.Hero
+import com.example.homework01.ui.screens.HeroDetailScreen
+import com.example.homework01.ui.screens.HeroListScreen
+
+@Composable
+fun MarvelApp(heroes: List<Hero>) {
+    MaterialTheme {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "hero_list") {
+            composable("hero_list") {
+                HeroListScreen(
+                    heroes = heroes,
+                    onHeroClick = { hero ->
+                        navController.navigate("hero_detail/${hero.name}")
+                    }
+                )
+            }
+            composable("hero_detail/{heroName}") { backStackEntry ->
+                val heroName = backStackEntry.arguments?.getString("heroName")
+                val hero = heroes.firstOrNull { it.name == heroName }
+                if (hero != null) {
+                    HeroDetailScreen(hero = hero, onBackClick = { navController.popBackStack() })
+                }
+            }
+        }
+    }
+}
